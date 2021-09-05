7501e9da4788a309b888da968be61cb20d1004d0
Brenton Turnor <brenton.turnor@hotmail.com>
2021-09-05 22:54:39 +0800

    updating user create and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Article, Category } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -11,6 +11,7 @@ router.post('/', async (req, res) => {
       });
   
       req.session.save(() => {
+        req.session.user_id = userData.id;
         req.session.loggedIn = true;
   
         res.status(200).json(userData);
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const articleRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
 
-router.use('/api', articleRoutes);
 router.use('/', homeRoutes);
+router.use('/api', articleRoutes);
 
 module.exports = router;
