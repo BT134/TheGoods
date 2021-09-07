e1727441200f7458c38ea332d4e2abd7337c21f1
Pasgen-Manley <pasgen95@gmail.com>
2021-09-07 18:40:35 +0800

    discover kind of done

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const articleRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+const newsRoutes = require('./newsApiRoute');
 
 router.use('/', homeRoutes);
+router.use('/news', newsRoutes);
 router.use('/api', articleRoutes);
 
 module.exports = router;
